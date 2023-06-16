e1de006cac47c4755bfbe5fd5803924faa978439
Sergey <Sergey@localhost.localdomain>
2023-06-16 23:36:40 +0300

    Remove unnecessary printed information, return more whole query result

--- dialog_flow.py
+++ dialog_flow.py
@@ -26,27 +26,15 @@ def get_df_api_key(project_id, suffix):
 
 def get_intent_from_text(project_id, session_id, text, language_code):
     session_client = dialogflow.SessionsClient()
-
     session = session_client.session_path(project_id, session_id)
-    print("Session path: {}\n".format(session))
 
     text_input = dialogflow.TextInput(text=text, language_code=language_code)
-
     query_input = dialogflow.QueryInput(text=text_input)
 
     response = session_client.detect_intent(
         request={"session": session, "query_input": query_input}
     )
-
-    print("=" * 20)
-    print("Query text: {}".format(response.query_result.query_text))
-    print(
-        "Detected intent: {} (confidence: {})\n".format(
-            response.query_result.intent.display_name,
-            response.query_result.intent_detection_confidence,
-        )
-    )
-    return response.query_result.fulfillment_text
+    return response.query_result
 
 
 if __name__ == '__main__':
