323a49f7b5a4649d7a7f6a71a0ea8e043b8306ac
Sergey <Sergey@localhost.localdomain>
2023-06-18 20:19:36 +0300

    Remove non-used function

--- dialog_flow.py
+++ dialog_flow.py
@@ -1,27 +1,5 @@
 from environs import Env
-from google.cloud import api_keys_v2
 from google.cloud import dialogflow
-
-
-def get_df_api_key(project_id, suffix):
-#    Create the API Keys client.
-    client = api_keys_v2.ApiKeysClient()
-
-    key = api_keys_v2.Key()
-    key.display_name = f'My first API key - {suffix}'
-
-    # Initialize request and set arguments.
-    request = api_keys_v2.CreateKeyRequest()
-    request.parent = f'projects/{project_id}/locations/global'
-    request.key = key
-
-    # Make the request and wait for the operation to complete.
-    response = client.create_key(request=request).result()
-
-    print(f'Successfully created an API key: {response.name}')
-    # For authenticating with the API key, use the value in "response.key_string".
-    # To restrict the usage of this API key, use the value in "response.name".
-    return response
 
 
 def get_intent_from_text(project_id, session_id, text, language_code):
